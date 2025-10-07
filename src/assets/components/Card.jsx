6fbe76889ac59615329b36825e9f81e337fe5cb6
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-10-07 14:40:30 -0700

    Dynamic Price

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 export default function Card({ item }) {
+    let price = item.price_usd 
+    if(price >= 70){
+        price = price / 2
+        price += " Sale!"
+    }
 
-  return (
-    <div>
-      <h2>{item.brand}</h2>
-      <h4>{item.name}</h4>
-      <p>{item.material}</p>
-      <p>{item.style}</p>
-      <p>{item.culture}</p>
-      <p>{item.price_usd}</p>
-      <hr/>
-    </div>
-  );
+    return (
+        <div>
+        <h2>{item.brand}</h2>
+        <h4>{item.name}</h4>
+        <p>{item.material}</p>
+        <p>{item.style}</p>
+        <p>{item.culture}</p>
+        <p>{price}</p>
+        <hr/>
+        </div>
+    );
 }

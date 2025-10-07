6f84d69587c8c1fa31841a21f2472d62272e8b04
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-10-07 14:20:07 -0700

    displaying all data

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -6,7 +6,11 @@ export default function Card({ item }) {
     <div>
       <h2>{item.brand}</h2>
       <h4>{item.name}</h4>
-      <hr />
+      <p>{item.material}</p>
+      <p>{item.style}</p>
+      <p>{item.culture}</p>
+      <p>{item.price_usd}</p>
+      <hr/>
     </div>
   );
 }

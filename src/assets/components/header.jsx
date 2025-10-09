c06898266a6d6fbd42a2cf2f43463e6eaf444403
JesseChum <jessechum@icloud.com>
2025-10-09 13:32:17 -0700

    fixed style of header and other css

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,25 +1,9 @@
-export function Header({ siteTitle = "Site", logoUrl, navLinks = [] }) {
+export function Header() {
   return (
     <header>
       <div>
-        <a href="/" aria-label={`${siteTitle} home`}>
-          {logoUrl ? (
-            <img src={logoUrl} alt={`${siteTitle} logo`} width="48" height="48" />
-          ) : (
-            <strong>{siteTitle}</strong>
-          )}
-        </a>
+       <p>JDM Hoodie Shop</p>
       </div>
-
-      <nav aria-label="Main navigation">
-        <ul>
-          {navLinks.map((link) => (
-            <li key={link.href}>
-              <a href={link.href}>{link.label}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
     </header>
   );
 }
